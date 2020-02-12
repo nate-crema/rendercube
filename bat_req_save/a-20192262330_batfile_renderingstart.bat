26a224c668178a16fbda816c0f0a44607b1304e1
nate-crema <nate.crema@gmail.com>
2020-02-12 23:15:26 +0900

    Sync with Github from USB_MAC

---
+++ bat_req_save/a-20192262330_batfile_renderingstart.bat
@@ -0,0 +1,8 @@
+echo off 
+title Rendercube-Rendering Status BAT File starttester_user_p20192262356
+
+echo Start Rendering System 
+echo. 
+echo. 
+cd 
+aerender -project C:\Users\user\Desktop\Rendercube\unzip\id\2019226233015\엠펙 로고.aep -comp "LOGO ANIMATION" -output C:\Users\user\Desktop\Rendercube\render_source_aep\20192262356_tester_user_p_LOGO ANIMATION.avi
